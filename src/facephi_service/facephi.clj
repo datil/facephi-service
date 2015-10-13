fb2b3c19982dd3804f629c365452988c66525f15
Eduardo Raad <eduardo.raad@datilmedia.com>
2015-10-13 11:07:10 -0500

    Implements best practices for biometric data management.

--- src/facephi_service/facephi.clj
+++ src/facephi_service/facephi.clj
@@ -2,7 +2,7 @@
   (:require [byte-streams :as b]
             [clojure.data.codec.base64 :as b64])
   (:import [com.facephi.sdk.matcher
-            Matcher MatcherException TemplateReliability
+            Matcher MatcherException TemplateReliability MatcherType
             MatchingSecurityLevel MatcherConfigurationManager]
            [com.facephi.sdk.licensing LicenseActivationException]))
 
@@ -14,41 +14,59 @@
 (defn registration-matcher-configuration
   []
   (doto (MatcherConfigurationManager.)
-    (.setTemplateReliability
-     TemplateReliability/ExtremeTemplateReliability)))
+    (.setTemplateReliability TemplateReliability/ExtremeTemplateReliability)
+    (.setMatcherType MatcherType/Any)))
 
-(defn auth-matcher-configuration
+(defn authentication-matcher-configuration
   []
   (doto (MatcherConfigurationManager.)
-    (.setMatchingSecurityLevel
-     MatchingSecurityLevel/HighSecurityLevel)))
+    (.setTemplateReliability TemplateReliability/ExtremeTemplateReliability)
+    (.setMatchingSecurityLevel MatchingSecurityLevel/HighSecurityLevel)
+    (.setMatcherType MatcherType/Any)))
+
+(defn manual-retrain-matcher-configuration
+  []
+  (doto (MatcherConfigurationManager.)
+    (.setTemplateReliability TemplateReliability/ExtremeTemplateReliability)
+    (.setMatchingSecurityLevel MatchingSecurityLevel/HighSecurityLevel)
+    (.setMatcherType MatcherType/Any)))
+
+(defn auto-retrain-matcher-configuration
+  []
+  (doto (MatcherConfigurationManager.)
+    (.setTemplateReliability TemplateReliability/ExtremeTemplateReliability)
+    (.setMatchingSecurityLevel MatchingSecurityLevel/VeryHighSecurityLevel)
+    (.setMatcherType MatcherType/Any)))
 
 (defn new-user
+  "Returns a new retrained user formatted as byte[]. Does an automatic retrain
+  with the second template if provided."
   ([face-1]
-   (try
-     (let [matcher (Matcher. (registration-matcher-configuration))
-           user (.createUser matcher face-1)]
-       user)
-     (catch MatcherException e
-       (throw (ex-info "FacePhi SDK: Error while generating new user."
-                       {}
-                       (.getCause e))))
-     (catch LicenseActivationException e
-       (throw (ex-info "FacePhi SDK: Licensing error."
-                       {}
-                       (.getCause e))))))
-  ([face-1 face-2]
    (let [matcher (Matcher. (registration-matcher-configuration))
-         user (.createUser matcher face-1)
-         retrained (.retrainUser matcher user face-2)]
+         user (.createUser matcher face-1)]
+     user))
+  ([face-1 face-2]
+   (let [registration-matcher (Matcher. (registration-matcher-configuration))
+         retrain-matcher (Matcher. (auto-retrain-matcher-configuration))
+         user (.createUser registration-matcher face-1)
+         retrained (.retrainUser retrain-matcher user face-2)]
      retrained)))
 
-(defn retrain
+(defn manual-retrain
+  "Manually retrains an user profile."
   [existing-face new-face]
-  (let [matcher (Matcher. (registration-matcher-configuration))]
+  (let [matcher (Matcher. (manual-retrain-matcher-configuration))]
+    (.retrainUser matcher existing-face new-face)))
+
+(defn auto-retrain
+  "Automatically retrains an user profile after registration or successful
+  authentication."
+  [existing-face new-face]
+  (let [matcher (Matcher. (auto-retrain-matcher-configuration))]
     (.retrainUser matcher existing-face new-face)))
 
 (defn authenticate
+  "Returns true or false whether the faces was authenticated or not."
   [user-face provided-face]
-  (let [matcher (Matcher. (auth-matcher-configuration))]
+  (let [matcher (Matcher. (authentication-matcher-configuration))]
     (.getIsPositiveMatch (.authenticate matcher user-face provided-face))))

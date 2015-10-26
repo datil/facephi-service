9a3ef3fb0c86b5e3462241e954f28cbdc088eecc
Eduardo Raad <eduardo.raad@datilmedia.com>
2015-10-26 11:39:21 -0500

    Saves logs in Oracle.

--- src/facephi_service/sql/user_log.sql
+++ src/facephi_service/sql/user_log.sql
@@ -1,4 +1,4 @@
 -- name: save-user-log!
 -- Creates a new log record
 
-INSERT INTO "USER_LOG" (username, transaction_type, transaction_result) VALUES (:username, :transaction_type, :transaction_result)
+INSERT INTO "USER_LOG" (username, identification, transaction_type, transaction_result) VALUES (:username, :identification, :transaction_type, :transaction_result)
